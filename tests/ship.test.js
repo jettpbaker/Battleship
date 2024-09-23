2c86440aad5d98398317155ede6a4b35e797af54
Jett <142007881+MilknCookys@users.noreply.github.com>
2024-09-23 18:27:25 +1000

    Created Ship object and tests for ship object.

---
+++ tests/ship.test.js
@@ -0,0 +1,30 @@
+import Ship from '../src/ship';
+
+test('Ship is correct length', () => {
+  const shortShip = new Ship(3);
+  expect(shortShip.length).toBe(3);
+  const longShip = new Ship(5);
+  expect(longShip.length).toBe(5);
+});
+
+test('Hit ship multiple times', () => {
+  const ship = new Ship(3);
+  ship.hit();
+  expect(ship.hits).toBe(1);
+  ship.hit();
+  expect(ship.hits).toBe(2);
+});
+
+test('Ship sinks when hits equal length', () => {
+  const ship = new Ship(3);
+  ship.hit();
+  ship.hit();
+  ship.hit();
+  expect(ship.isSunk()).toBe(true);
+});
+
+test("Ship sinks when hits don't equal length", () => {
+  const ship = new Ship(3);
+  ship.hit();
+  expect(ship.isSunk()).toBe(false);
+});
